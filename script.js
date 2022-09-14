b689d5e74898a38817096f6258cc7e1ccc1b7a7f
Leonardo Espindula <espindulasystem.dev@gmail.com>
2022-09-14 20:53:43 -0300

    update

--- script.js
+++ script.js
@@ -3,166 +3,127 @@
         document.querySelector('.popup-image#_1').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+   
     document.querySelectorAll('.formacao li#_2').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_2').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_3').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_3').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_4').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_4').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_5').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_5').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_6').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_6').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+   
     document.querySelectorAll('.formacao li#_7').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_7').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_8').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_8').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_9').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_9').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_10').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_10').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+   
     document.querySelectorAll('.formacao li#_11').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_11').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_12').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_12').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_13').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_13').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_14').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_14').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_15').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_15').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_16').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_16').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_17').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_17').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_18').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_18').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_19').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_19').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_20').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_20').style.display = 'block';
         }
     })
-    document.querySelector('.popup-image span').onclick = () =>{
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+    
     document.querySelectorAll('.formacao li#_21').forEach(image => {
         image.onclick = () =>{
         document.querySelector('.popup-image#_21').style.display = 'block';
         }
     })
+    
     document.querySelector('.popup-image span').onclick = () =>{
         document.querySelector('.popup-image').style.display = 'none';
     }
